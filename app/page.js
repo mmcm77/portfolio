73f6ce8d5e219ff89e49fed87540dc5be627483f
Mike <62350021+mmcm77@users.noreply.github.com>
2023-05-15 21:18:25 -0400

    temp home page

--- app/page.js
+++ app/page.js
@@ -1,21 +1,28 @@
+import Image from "next/image"
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
 
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://mikemcmillan.ca"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Mike McMillan
-          </a>
-        </div>
+export default function About() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-6">
+ <div className="flex flex-col items-center mt-8">
+
+      <h1 className="text-3xl font-bold mb-2">About Me</h1>
+      <p className="text-lg text-center">
+        Hi there! I'm Mike, and this is my playground for learning. 
+      </p>
+
+      <div className="flex justify-center mt-6 space-x-4">
+        <a href="https://www.linkedin.com/in/johndoe" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin text-4xl text-blue-500 hover:text-blue-700"></i>
+        </a>
+        <a href="https://github.com/johndoe" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github text-4xl text-gray-600 hover:text-gray-800"></i>
+        </a>
       </div>
+    </div>
 
-    </main>
-  )
-}
+  
+      </main>
+    )
+  }
+  
